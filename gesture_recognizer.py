cc0edb68166035cdd0bb7e778d43b85f6ba35974
Leviasm <levi.asmussen@stud.hs-flensburg.de>
2025-01-04 13:49:41 +0100

    Added simple data input and plot function for testing

--- gesture_recognizer.py
+++ gesture_recognizer.py
@@ -1,1 +1,33 @@
 #Write gesture recognition here
+import os
+
+import pandas as pd
+import plotly.graph_objs as go
+
+
+def data_input(filename):
+    try:
+        dataframe = pd.read_csv(os.getcwd() + filename, delimiter=',')
+        print("Data loaded successfully")
+        print(dataframe.head())
+        return dataframe
+
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
+
+
+'''Funktion erstmal zum Visualisieren, um zu checken ob die Bereinigung funktioniert hat'''
+def plot_filter(data, title):
+    fig = go.Figure()
+    for column in data.columns:
+        fig.add_trace(go.Scatter(x=data.index, y=data[column], mode='lines', name=column))
+
+    fig.update_layout(title=title,
+                      xaxis_title='Zeit',
+                      yaxis_title='Werte')
+    fig.show()
+
+
+daten = data_input("/datasets/circle/circle_accelerometer.csv")
+plot_filter(daten, "Input")
